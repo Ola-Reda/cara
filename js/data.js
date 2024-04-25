ac482d8ee2aedc43e7f8f7e6047b0d4d63ac638e
Ola-Reda <ola.reda.900@gmail.com>
2024-04-25 16:27:51 +0200

    Add project

---
+++ js/data.js
@@ -0,0 +1,254 @@
+const featureData = [
+    {
+        id: 1,
+        name: 'free shipping',
+        image: 'images/features/f1.png',
+    },
+    {
+        id: 2,
+        name: 'online order',
+        image: 'images/features/f2.png',
+    },
+    {
+        id: 3,
+        name: 'save money',
+        image: 'images/features/f3.png',
+    },
+    {
+        id: 4,
+        name: 'promations',
+        image: 'images/features/f4.png',
+    },
+    {
+        id: 5,
+        name: 'happy sall',
+        image: 'images/features/f5.png',
+    },
+    {
+        id: 6,
+        name: 'f24/7 support',
+        image: 'images/features/f6.png',
+    },
+]
+
+
+const productsData = [
+    {
+        id: 1,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f1.jpg',
+        brand: 'adidas',
+        rating: 5,
+        price: 66,
+    },
+    {
+        id: 2,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f2.jpg',
+        brand: 'adidas',
+        rating: 4,
+        price: 88,
+    },
+    {
+        id: 3,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f3.jpg',
+        brand: 'adidas',
+        rating: 3,
+        price: 100,
+    },
+    {
+        id: 4,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f4.jpg',
+        brand: 'adidas',
+        rating: 2,
+        price: 50,
+    },
+    {
+        id: 5,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f5.jpg',
+        brand: 'adidas',
+        rating: 3,
+        price: 78,
+    },
+    {
+        id: 6,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f6.jpg',
+        brand: 'adidas',
+        rating: 3,
+        price: 60,
+    },
+    {
+        id: 7,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f7.jpg',
+        brand: 'adidas',
+        rating: 5,
+        price: 88,
+    },
+    {
+        id: 8,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/f8.jpg',
+        brand: 'adidas',
+        rating: 4,
+        price: 100,
+    },
+]
+
+
+const arrivalData = [
+    {
+        id: 9,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n1.jpg',
+        brand: 'adidas',
+        rating: 5,
+        price: 66,
+    },
+    {
+        id: 10,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n2.jpg',
+        brand: 'adidas',
+        rating: 4,
+        price: 88,
+    },
+    {
+        id: 11,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n3.jpg',
+        brand: 'adidas',
+        rating: 3,
+        price: 100,
+    },
+    {
+        id: 12,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n4.jpg',
+        brand: 'adidas',
+        rating: 2,
+        price: 50,
+    },
+    {
+        id: 13,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n5.jpg',
+        brand: 'adidas',
+        rating: 3,
+        price: 78,
+    },
+    {
+        id: 14,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n6.jpg',
+        brand: 'adidas',
+        rating: 3,
+        price: 60,
+    },
+    {
+        id: 15,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n7.jpg',
+        brand: 'adidas',
+        rating: 5,
+        price: 88,
+    },
+    {
+        id: 16,
+        name: 'cartoon astronaut t-shirt',
+        image: 'images/products/n8.jpg',
+        brand: 'adidas',
+        rating: 4,
+        price: 100,
+    },
+]
+
+
+const bannersTopData = [
+    {
+        id: 1,
+        name: 'crazy deals',
+        image: 'images/banners/b17.jpg',
+        sale: 'buy 1 get 1 free',
+        desc: 'the best classic dress is on sale at cara',
+        more: 'learn more',
+    },
+    {
+        id: 2,
+        name: 'spring/summer',
+        image: 'images/banners/b10.jpg',
+        sale: 'upcoming season',
+        desc: 'the best classic dress is on sale at cara',
+        more: 'collection',
+    },
+]
+
+
+const bannersBottomData = [
+    {
+        id: 3,
+        name: 'seasonal sale',
+        image: 'images/banners/b7.jpg',
+        desc: 'winter collection -50% off',
+    },
+    {
+        id: 4,
+        name: 'new footwear collection',
+        image: 'images/banners/b4.jpg',
+        desc: 'spring/summer 2024',
+    },
+    {
+        id: 5,
+        name: 't-shirts',
+        image: 'images/banners/b18.jpg',
+        desc: 'new trendy prints',
+    },
+]
+
+
+const blogsData = [
+    {
+        id: 1,
+        name: 'The Cotton-Jersey Zip-Up Hoodie',
+        image: 'images/blog/b1.jpg',
+        desc: 'Kickstarter man braid godard calaring book. Raclette waistcoat selfies yr woll chartreuse hexagon irony. godord',
+        more: 'continue reading',
+    },
+    {
+        id: 2,
+        name: 'How To style A Quiff',
+        image: 'images/blog/b2.jpg',
+        desc: 'Kickstarter man braid godard calaring book. Raclette waistcoat selfies yr woll chartreuse hexagon irony. godord',
+        more: 'continue reading',
+    },
+    {
+        id: 3,
+        name: 'Must-have Skater Girl Items',
+        image: 'images/blog/b3.jpg',
+        desc: 'Kickstarter man braid godard calaring book. Raclette waistcoat selfies yr woll chartreuse hexagon irony. godord',
+        more: 'continue reading',
+    },
+    {
+        id: 4,
+        name: 'Runway-Inspired Trends',
+        image: 'images/blog/b4.jpg',
+        desc: 'Kickstarter man braid godard calaring book. Raclette waistcoat selfies yr woll chartreuse hexagon irony. godord',
+        more: 'continue reading',
+    },
+    {
+        id: 5,
+        name: 'The Cotton-Jersey Zip-Up Hoodie',
+        image: 'images/blog/b5.jpg',
+        desc: 'Kickstarter man braid godard calaring book. Raclette waistcoat selfies yr woll chartreuse hexagon irony. godord',
+        more: 'continue reading',
+    }
+]
+
+
+localStorage.setItem("products", JSON.stringify(productsData))
+localStorage.setItem("products2", JSON.stringify(arrivalData))
+
